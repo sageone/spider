2c72ba5b8bbf5cacf1524f3753b1c18a0e56cd62
david.frigola@gmail.com <david.frigola@gmail.com@4bad15f0-1fa9-d28a-80b3-2b18e0056ab6>
2010-12-19 17:04:22 +0000

    Change the test site -> change the configuration for the base spider test. Removed warnings (non used imports)

--- spider/src/test/java/org/nms/spider/engine/BaseSpiderTest.java
+++ spider/src/test/java/org/nms/spider/engine/BaseSpiderTest.java
@@ -1,21 +1,10 @@
 package org.nms.spider.engine;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.nms.spider.beans.IElement;
-import org.nms.spider.beans.impl.StringElement;
 import org.nms.spider.engine.impl.BaseSpider;
-import org.nms.spider.helpers.impl.AppenderElementModifierImpl;
-import org.nms.spider.helpers.impl.RegexParserProcessorImpl;
-import org.nms.spider.helpers.impl.UrlProcessorImpl;
-import org.nms.spider.helpers.utils.ChainedProcessorHelperImpl;
-import org.nms.spider.helpers.utils.ElementModifierProcessorImpl;
-import org.nms.spider.helpers.utils.LogProcessorImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,35 +12,34 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * This test must be used on a controlled web site.
- * The web site is under /var/www/test/spider.
+ * This test must be used on a controlled web site. The web site is under
+ * /var/www/test/spider.
+ * 
  * @author daviz
- *
+ * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath:*testContext-baseSpiderTest.xml")
+@ContextConfiguration(locations = "classpath:*testContext-baseSpiderTest.xml")
 public class BaseSpiderTest {
 
 	private static final Logger log = LoggerFactory.getLogger(BaseSpider.class);
-	
+
 	@Autowired
 	BaseSpider spider;
-	
-	@Before
-	public void before(){
 
-		Assert.assertNotNull("The spider is null!",spider);
-		log.debug("The spider loaded, with {} preprocessors and {} processors"
-				,spider.getPreprocessor(),spider.getPostprocessor());
+	@Before
+	public void before() {
+
+		Assert.assertNotNull("The spider is null!", spider);
+		log.debug("The spider loaded, with {} preprocessors and {} processors",
+				spider.getPreprocessor(), spider.getPostprocessor());
 	}
 
-	
 	@Test
-	public void spiderTest(){
-		
+	public void spiderTest() {
+
 		spider.spider();
-		
-		
+
 	}
 
 }
