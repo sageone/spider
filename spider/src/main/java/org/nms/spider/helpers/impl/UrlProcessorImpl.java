415d6db6378d63f6996dffa12e1a5c0237992d46
david.frigola@gmail.com <david.frigola@gmail.com@4bad15f0-1fa9-d28a-80b3-2b18e0056ab6>
2011-01-03 19:57:06 +0000

    Add the ID for the elements obtained using this processor.

--- spider/src/main/java/org/nms/spider/helpers/impl/UrlProcessorImpl.java
+++ spider/src/main/java/org/nms/spider/helpers/impl/UrlProcessorImpl.java
@@ -9,7 +9,6 @@ import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import org.nms.spider.beans.IElement;
 import org.nms.spider.beans.impl.TypedElement;
 import org.nms.spider.helpers.AbstractProcessor;
@@ -17,64 +16,60 @@ import org.nms.spider.helpers.IProcessorHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class UrlProcessorImpl 
-extends AbstractProcessor
-implements IProcessorHelper {
+public class UrlProcessorImpl extends AbstractProcessor implements
+		IProcessorHelper {
 
-	private static final Logger log = LoggerFactory.getLogger(UrlProcessorImpl.class);
+	private static final Logger log = LoggerFactory
+			.getLogger(UrlProcessorImpl.class);
 
-	
-	
 	@SuppressWarnings("rawtypes")
 	@Override
 	public List<IElement> process(List<IElement> elements) {
 		ArrayList<IElement> result = new ArrayList<IElement>();
-		
-		for(IElement el:elements){
-			
-			String urlString = (String) el.getElement();
-			
-			log.info("Preprocessing url : "+urlString);
-			
-			TypedElement te = new TypedElement();
-			te.setType("htmlcontent");
-			
 
-			try{
+		for (IElement el : elements) {
+
+			String urlString = (String) el.getElement();
+
+			log.info("Preprocessing url : " + urlString);
+
+			TypedElement te = new TypedElement();
+			te.setId(urlString);
+			te.setType("htmlcontent");
+
+			try {
 				URL url = new URL(urlString);
 				URLConnection connection = url.openConnection();
-			    if ( (connection.getContentType()!=null) &&
-			           !connection.getContentType().toLowerCase()
-			                      .startsWith("text/") ) {
-			        
-			        
-			        
-			    }
-			    InputStream is = connection.getInputStream();
-			    Reader r = new InputStreamReader(is);
-			    CharBuffer cbuff = CharBuffer.allocate(1000);
-			    StringBuffer sb = new StringBuffer();
-			    while(r.read(cbuff)!=-1){
-				    
-			    	log.trace("CBUFF-"+cbuff.length());
-			    	
-			    	for(int i=0;i<1000-cbuff.length();i++){
-			    		sb.append(cbuff.get(i));
-			    	}
-			    	sb.append(cbuff.toString());
-			    
-			    	cbuff = CharBuffer.allocate(1000);
-			    }
-			    log.trace("SB:"+sb.toString());
-			    
-			    te.setElement(sb.toString());
-			    result.add(te);
-			}catch(Exception e){
-				log.error("Error trying to process the URL.",e);
-				
+				if ((connection.getContentType() != null)
+						&& !connection.getContentType().toLowerCase()
+								.startsWith("text/")) {
+
+				}
+				InputStream is = connection.getInputStream();
+				Reader r = new InputStreamReader(is);
+				CharBuffer cbuff = CharBuffer.allocate(1000);
+				StringBuffer sb = new StringBuffer();
+				while (r.read(cbuff) != -1) {
+
+					log.trace("CBUFF-" + cbuff.length());
+
+					for (int i = 0; i < 1000 - cbuff.length(); i++) {
+						sb.append(cbuff.get(i));
+					}
+					sb.append(cbuff.toString());
+
+					cbuff = CharBuffer.allocate(1000);
+				}
+				log.trace("SB:" + sb.toString());
+
+				te.setElement(sb.toString());
+				result.add(te);
+			} catch (Exception e) {
+				log.error("Error trying to process the URL.", e);
+
 			}
 		}
-		
+
 		return result;
 	}
 
