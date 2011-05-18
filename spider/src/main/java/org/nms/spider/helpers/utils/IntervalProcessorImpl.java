860201a7277c13d36a81527b7f8c28611f05b7df
david.frigola@gmail.com <david.frigola@gmail.com@4bad15f0-1fa9-d28a-80b3-2b18e0056ab6>
2011-05-18 16:43:32 +0000

    Interval between element processing. NOT TESTED. Intended use : human like url navigation (for URL processors or FileDownloaders)

---
+++ spider/src/main/java/org/nms/spider/helpers/utils/IntervalProcessorImpl.java
@@ -0,0 +1,127 @@
+package org.nms.spider.helpers.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.nms.spider.beans.IElement;
+import org.nms.spider.helpers.AbstractProcessor;
+import org.nms.spider.helpers.IProcessorHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Adds a waiting interval time after each element processed. 
+ * 
+ * This interval can be fix or random [minMilisPeriod - maxMilisPeriod] (default [0-1000] ) 
+ * <p>
+ * The interval is measured in miliseconds.
+ * </p>
+ * 
+ * TODO Test this processor.
+ * 
+ * @author daviz
+ *
+ */
+public class IntervalProcessorImpl 
+extends AbstractProcessor
+implements IProcessorHelper{
+
+	/**
+	 * The logger.
+	 */
+	private final static Logger log = LoggerFactory.getLogger(IntervalProcessorImpl.class);
+	
+	private long minMilisPeriod = 0;
+	
+	/**
+	 * The maximum time sleepint between element processing, in miliseconds.
+	 */
+	private long maxMilisPeriod = 1000;
+	
+	/**
+	 * FLAG for random period. The random period is in range [0-maxMilisPerior]
+	 */
+	private boolean randomPeriod = Boolean.FALSE;
+	
+	/**
+	 * The processor for the elements.
+	 */
+	private IProcessorHelper processor;
+	
+	public void IntervalProcessor(IProcessorHelper processor){
+		
+		this.processor = processor;
+		
+	}
+	
+	@Override
+	public List<IElement> process(List<IElement> elements) {
+		
+		log.debug("Starting interval processor. Random [ {} ] range [ {} ]",randomPeriod,minMilisPeriod+"-"+maxMilisPeriod);
+		
+		List<IElement> result = new ArrayList<IElement>();
+		
+		for(IElement e:elements){
+			result = processor.process(e);
+			
+			try{
+				sleep();
+			}catch(InterruptedException ie){
+				// nothing to do!
+				log.warn("Exception during sleep!",e);
+			}
+		}
+		return result;
+	}
+
+	public long getMinMilisPeriod() {
+		return minMilisPeriod;
+	}
+
+	public void setMinMilisPeriod(long minMilisPeriod) {
+		this.minMilisPeriod = minMilisPeriod;
+	}
+
+	public long getMaxMilisPeriod() {
+		return maxMilisPeriod;
+	}
+
+	public void setMaxMilisPeriod(long maxMilisPeriod) {
+		this.maxMilisPeriod = maxMilisPeriod;
+	}
+
+	public IProcessorHelper getProcessor() {
+		return processor;
+	}
+
+	public void setProcessor(IProcessorHelper processor) {
+		this.processor = processor;
+	}
+	
+	public boolean isRandomPeriod() {
+		return randomPeriod;
+	}
+
+	public void setRandomPeriod(boolean randomPeriod) {
+		this.randomPeriod = randomPeriod;
+	}
+
+	private void sleep() throws InterruptedException{
+		
+		// Calculate range absolute value.
+		long sleepPeriod = this.getMaxMilisPeriod() - this.getMinMilisPeriod();
+		
+		// Random flag
+		if(randomPeriod){
+			sleepPeriod = (long) Math.floor(sleepPeriod * Math.random());
+			
+		}
+		// Normalize due min value
+		sleepPeriod = sleepPeriod + this.getMinMilisPeriod();
+		
+		log.debug("Random[ {} ] sleep time : ",randomPeriod,sleepPeriod);
+		
+		Thread.sleep(sleepPeriod);
+	}
+
+}
