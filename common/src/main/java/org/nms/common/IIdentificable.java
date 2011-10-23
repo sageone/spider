6b65a70d2c6fd9a1c4629a7f3c4b5e2ce50ad7d2
david.frigola@gmail.com <david.frigola@gmail.com@4bad15f0-1fa9-d28a-80b3-2b18e0056ab6>
2011-10-23 18:20:45 +0000

    Added some javadoc

--- common/src/main/java/org/nms/common/IIdentificable.java
+++ common/src/main/java/org/nms/common/IIdentificable.java
@@ -2,15 +2,17 @@ package org.nms.common;
 
 /**
  * Defines a identificable interface to compare to elements.
+ * 
  * @author daviz
- *
- * @param <PK> The primary key.
+ * 
+ * @param <PK>
+ *            The primary key.
  */
 public interface IIdentificable<PK> {
 
 	public PK getId();
-	
+
 	public void setId(PK id);
-	
+
 	public boolean isequals(PK id);
 }
